48736a8761f653cf63db0d6a7c16cf979ce9ffaf
MkP369 <kritarthprasad01@gmail.com>
2026-06-30 10:35:04 +0530

    fix condition in lock() and make vars public

--- src/WorkerContext.hpp
+++ src/WorkerContext.hpp
@@ -1,4 +1,5 @@
-#include "utils/PathString.hpp"
+#pragma once
+#include "../utils/PathString.hpp"
 #include <array>
 #include <atomic>
 #include <cstdint>
@@ -16,16 +17,37 @@ struct linux_dirent64 {
 
 class WorkerContext {
 public:
-  WorkerContext()
-      : m_local_dir_queue(1024) {} // preallocate to prevent
-                                   // resizing
+  static constexpr uint32_t GETDENTS_BUF_SZ =
+      256 * 1024; // 256 kb to fit in L2 Cache
+
+  std::atomic_flag spinlock = ATOMIC_FLAG_INIT;
+  std::vector<PathString> local_dir_queue;
+
+  // aligned because another thread can invalidate
+  // total_blocks cache for this thread while stealing
+  // (accessing both spinlock and local_dir_queue)
+  alignas(std::hardware_destructive_interference_size) uint64_t total_blocks =
+      0;
+  // aligned to the page size to make reads fast
+  alignas(4096) std::array<uint8_t, GETDENTS_BUF_SZ> dents_buf;
+
+  // A stack-allocated buffer used to construct full child paths
+  // linux path names length have a max limit of 4096 chars
+  // else the syscalls give long name errors
+  std::array<char, 4096>
+      path_buf; // for storing current file paths to be put into queue
+
+  WorkerContext() {
+    // preallocate to prevent resizing
+    local_dir_queue.reserve(1024);
+  }
 
   [[gnu::always_inline]]
   bool try_take(PathString &out) {
     lock();
-    if (!m_local_dir_queue.empty()) {
-      out = std::move(m_local_dir_queue.back());
-      m_local_dir_queue.pop_back();
+    if (!local_dir_queue.empty()) {
+      out = std::move(local_dir_queue.back());
+      local_dir_queue.pop_back();
       unlock();
       return true;
     }
@@ -36,36 +58,21 @@ public:
   [[gnu::always_inline]]
   void push_back(PathString &dir) {
     lock();
-    m_local_dir_queue.push_back(std::move(dir));
+    local_dir_queue.push_back(std::move(dir));
     unlock();
   }
 
   [[gnu::always_inline]]
   void emplace_back(const char *path, uint32_t len) {
     lock();
-    m_local_dir_queue.emplace_back(path, len);
+    local_dir_queue.emplace_back(path, len);
     unlock();
   }
 
 private:
-  static const uint32_t GETDENTS_BUF_SZ =
-      256 * 1024; // 256 kb to fit in L2 Cache
-
-  std::atomic_flag m_spinlock = ATOMIC_FLAG_INIT;
-  std::vector<PathString> m_local_dir_queue;
-
-  // aligned because another thread can invalidate
-  // total_blocks cache for this thread while stealing
-  // (accessing both spinlock and local_dir_queue)
-  alignas(std::hardware_destructive_interference_size) uint64_t m_total_blocks =
-      0;
-
-  // aligned to the page size to make reads fast
-  alignas(4096) std::array<uint8_t, GETDENTS_BUF_SZ> m_buf;
-
   [[gnu::always_inline]]
   void lock() noexcept {
-    while (!m_spinlock.test_and_set(std::memory_order_acquire)) [[likely]] {
+    while (spinlock.test_and_set(std::memory_order_acquire)) [[likely]] {
       // If the lock is held by someone else, we must yield the CPU to prevent
       // burning cycles.
 #if defined(__x86_64__) || defined(__i386__)
@@ -77,6 +84,6 @@ private:
   }
   [[gnu::always_inline]]
   void unlock() noexcept {
-    m_spinlock.clear(std::memory_order_release);
+    spinlock.clear(std::memory_order_release);
   }
 };
